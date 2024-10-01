b2d50ade020621c4c9fe951b3de9e26dace215c6
yudhiana99 <yudiyana@mbiz.co.id>
2024-10-01 15:37:19 +0700

    Update config.go

--- rmq/config.go
+++ rmq/config.go
@@ -7,8 +7,8 @@ import (
 
 func GetRabbitURL() string {
 	return fmt.Sprintf("amqp://%s:%s@%s:%s/",
-		os.Getenv("RABBIT_HOST"),
-		os.Getenv("RABBIT_PORT"),
 		os.Getenv("RABBIT_USER"),
-		os.Getenv("RABBIT_PASSWORD"))
+		os.Getenv("RABBIT_PASSWORD"),
+		os.Getenv("RABBIT_HOST"),
+		os.Getenv("RABBIT_PORT"))
 }

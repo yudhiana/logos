f01cad5154a325b7c9b4bb4d9bc48ddf89e46a87
yudhiana99 <yudiyana@mbiz.co.id>
2024-10-01 13:13:41 +0700

    Update api_request.go, api_response.go, and tracer_request.go

--- tracer/models/api_request.go
+++ tracer/models/api_request.go
@@ -9,6 +9,7 @@ type APIRequest struct {
 	RequestID    string      `json:"request_id"`
 	Status       int         `json:"status"`
 	Method       string      `json:"method"`
+	Type         string      `json:"type"`
 	URL          string      `json:"url"`
 	ClientIP     string      `json:"client_ip"`
 	UserAgent    string      `json:"user_agent"`

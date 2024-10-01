4eb8ae7738cd825625f3097f6d80efabe12c5e32
yudhiana99 <yudiyana@mbiz.co.id>
2024-10-01 18:45:14 +0700

    Update tracing_request.go

--- middleware/tracing_request.go
+++ middleware/tracing_request.go
@@ -10,12 +10,12 @@ import (
 
 func TraceIncomingRequest(ctx iris.Context) {
 	tracer.AuthenticateRequestId(ctx)
-	go func(irisCtx iris.Context) {
+	// go func(irisCtx iris.Context) {
 		observable := observer.NewObservable()
-		goCtx := context.WithValue(context.Background(), tracer.IrisContextKey, irisCtx)
+		goCtx := context.WithValue(context.Background(), tracer.IrisContextKey, ctx)
 		observable.Register(observer.NewObserver("tracer request", tracer.TracerIncomingRequest))
 		observable.TriggerEvent("tracer request", goCtx)
-	}(ctx)
+	// }(ctx)
 	ctx.Next()
 }
 

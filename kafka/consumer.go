8b412c7f57016df8e4d957e9d5b2a22b22389481
yudhiana99 <yudiyana@mbiz.co.id>
2024-11-21 14:12:13 +0700

    - implement process retry to connect kafka when failed  try to connection

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -2,7 +2,8 @@ package kafka
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"time"
 
 	"github.com/IBM/sarama"
 	"github.com/mataharibiz/ward/logging"
@@ -26,56 +27,44 @@ type ConsumerGroup struct {
 // Closing the consumer group or canceling the context will cause the function to return.
 // The function is designed to be run in a goroutine, so it will not block the calling goroutine.
 func NewKafkaConsumerGroup(cg *ConsumerGroup, handler Handler) {
-
-	// Set up the Kafka consumer group
-	client, errConsumer := GetConsumerGroup(cg)
-	if errConsumer != nil {
-		logging.NewLogger().Error("kafka consumer error", "error", errConsumer)
-		return
-	}
-	defer client.Close()
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go func() {
-		for {
-			defer Recover()
+	retryInterval := 5 * time.Second
+	maxRetries := -1
+	backoffFactor := 2.0
 
-			if err := client.Consume(ctx, cg.Topics, NewKafkaHandler(handler, client)); err != nil {
-				if errors.Is(err, sarama.ErrClosedConsumerGroup) {
-					return
-				}
+	for attempt := 1; maxRetries == -1 || attempt <= maxRetries; attempt++ {
+		logging.NewLogger().Info(fmt.Sprintf("Attempt %d to connect to Kafka...", attempt))
 
-				logging.NewLogger().Error("kafka consumer error", "error", err)
+		// set up the Kafka consumer group
+		client, errConsumer := GetConsumerGroup(cg)
+		if errConsumer != nil {
+			logging.NewLogger().Error("failed to create kafka consumer", "error", errConsumer)
+
+			if maxRetries != -1 && attempt == maxRetries {
 				return
 			}
 
-			if ctx.Err() != nil {
-				return
-			}
-		}
-	}()
-
-	active := true
-	for active {
-		logging.NewLogger().Info("Listening for kafka messages")
-		select {
-		case <-WaitForSignal():
-			logging.NewLogger().Warn("shutting down", "operation", "kafka-consumer-disconnect")
-			if err := client.Close(); err != nil {
-				logging.NewLogger().Error("failed to close kafka consumer", "error", err)
-			}
-			active = false
-		case <-ctx.Done():
-			logging.NewLogger().Info("shutting down", "operation", "kafka-consumer-disconnect")
-			if err := client.Close(); err != nil {
-				logging.NewLogger().Error("failed to close kafka consumer", "error", err)
-			}
-			active = false
+			// exponential backoff
+			sleepDuration := retryInterval * time.Duration(attempt) * time.Duration(backoffFactor)
+			time.Sleep(sleepDuration)
+			continue
 		}
 
+		logging.NewLogger().Info("Connected to Kafka successfully")
+		defer client.Close()
+
+		go cg.consumerMessage(ctx, client, handler)
+
+		go cg.handleGracefulShutdown(ctx, client)
+
+		// reconnect in case of failure
+		logging.NewLogger().Info("Reconnecting to Kafka...")
+		time.Sleep(retryInterval)
+		return
 	}
+
 }
 
 // GetConsumerGroup creates a new Kafka consumer group with the given groupID and hosts.
@@ -108,4 +97,36 @@ func GetConsumerGroup(cg *ConsumerGroup) (sarama.ConsumerGroup, error) {
 		return nil, errConsumer
 	}
 	return consumer, nil
+}
+
+func (cg *ConsumerGroup) consumerMessage(ctx context.Context, client sarama.ConsumerGroup, handler Handler) {
+	defer Recover()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			logging.NewLogger().Info("Listening for kafka messages")
+			err := client.Consume(ctx, cg.Topics, NewKafkaHandler(handler, client))
+			if err != nil {
+				logging.NewLogger().Error("kafka consumer error", "error", err)
+			}
+		}
+	}
+}
+
+func (cg *ConsumerGroup) handleGracefulShutdown(ctx context.Context, client sarama.ConsumerGroup) {
+	select {
+	case <-WaitForSignal():
+		logging.NewLogger().Warn("shutting down", "operation", "kafka-consumer-disconnect")
+		if err := client.Close(); err != nil {
+			logging.NewLogger().Error("failed to close kafka consumer", "error", err)
+		}
+
+	case <-ctx.Done():
+		logging.NewLogger().Info("shutting down", "operation", "kafka-consumer-disconnect")
+		if err := client.Close(); err != nil {
+			logging.NewLogger().Error("failed to close kafka consumer", "error", err)
+		}
+	}
 }

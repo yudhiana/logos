a8aec1ab0f91a0eedbe72f39978bcfec50492f98
yudhiana <yudiyana@mbiz.co.id>
2025-06-19 11:47:42 +0700

    feat : [mataharibiz/generic-project#1004] - add module JSON sanitizer

--- json_sanitizer/sanitize_test.go
+++ json_sanitizer/sanitize_test.go
@@ -44,7 +44,9 @@ func TestIsJSON(t *testing.T) {
 }
 
 func TestSanitizeStringJSON(t *testing.T) {
-	var jsonString = `{"history":[{"auth":"secret","event":"login","timestamp":"2025-06-19 11:17:31.569680128 +0700 WIB m=+0.002512345","token":"my_secret"}],"password":"my_secret_1750306651569652039","profile":{"auth":"my_secret_1750306651569654189","details":{"device_id":"device_1750306651569679454","ip":"127.0.0.1"},"email":"user_1750306651569652599@example.com","otp":"my_secret_1750306651569653943","pin":"1750306651569654713","secret":"my_secret_1750306651569653347","token":"my_secret_1750306651569653052"},"user_id":1750306651,"username":"user_1750306651569649343"}`
+	jsonRaw := GenerateDeepJSON()
+	jsonByte, _ := json.Marshal(jsonRaw)
+	jsonString := string(jsonByte)
 
 	sanitizer := NewJsonSanitizer()
 	sanitized := sanitizer.Sanitize(jsonString)
